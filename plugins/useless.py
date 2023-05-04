4326df24f137162790a355daa31fe8a71ce169bc
filesstoree <130815410+filesstoree@users.noreply.github.com>
2023-05-04 20:32:29 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -5,6 +5,21 @@ from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
 
+
+@Bot.on_message(filters.command('followus'))
+async def followus(bot: Bot, message: Message):
+    while True:
+         reply_markup=InlineKeyboardMarkup(
+                        [
+                         [
+                          InlineKeyboardButton('𝓜𝓸𝓿𝓲𝓮𝓼 𝓖𝓻𝓸𝓾𝓹', url="t.me/+ADvUFRV3nsljNTM1"),
+                          InlineKeyboardButton('𝓤𝓹𝓭𝓪𝓽𝓮𝓼 𝓒𝓱𝓪𝓷𝓷𝓮𝓵', url="t.me/MoviezAddaKan")
+                       ],[
+                          InlineKeyboardButton("𝓑𝓸𝓽 𝓞𝔀𝓷𝓮𝓻", url="t.me/captblacknight")
+                         ]
+                        ]
+                    )
+
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
     now = datetime.now()
@@ -17,3 +32,5 @@ async def stats(bot: Bot, message: Message):
 async def useless(_,message: Message):
     if USER_REPLY_TEXT:
         await message.reply(USER_REPLY_TEXT)
+        
+        
