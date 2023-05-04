cb001de5dba386a6bd60e9b4077362dd1fd1bddf
filesstoree <130815410+filesstoree@users.noreply.github.com>
2023-05-04 21:08:10 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -19,7 +19,7 @@ async def followus(bot: Bot, message: Message):
                          ]
                         ]
                     )
-    await reply_text.edit(f"<b>FOllow us here </b>\n\n", reply_markup=reply_markup, disable_web_page_preview = True)
+    await message.reply(f"<b>FOllow us here </b>\n\n", reply_markup=reply_markup, disable_web_page_preview = True)
 
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
